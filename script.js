32c3c9b29e0e613536162326cda46f62da5fefd1
EGAProg <gameswithiy@gmail.com>
2023-01-24 10:05:24 +0300

    First task

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-console.log("Hello World!");
+"use strict";
 
+const flats = 4,
+ floors = 4, 
+ enterance = 3;
+
+console.log("Number of flats in whole building:", flats*floors*enterance);
+
+console.log("Number of flats in one entarance:", flats*floors);
