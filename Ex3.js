932f5c6236af1f3bbfa54c3c069180253e71b486
Egor <93545142+EGAProg@users.noreply.github.com>
2023-01-30 19:22:13 +0300

    Добавил коментарии

--- Ex3.js
+++ Ex3.js
@@ -2,7 +2,7 @@
 const number1 = Math.random() * 100;
 const number2 = Math.random() * 100;
 console.log("First num:",number1,"\nSecond num:",number2);
-let average = (number1 + number2) / 2;
-let Frac =(number1 + number2) / 2 % 1;
+let average = (number1 + number2) / 2; // Среднее арефмитическое
+let Frac =(number1 + number2) / 2 % 1; // Дробная часть
 
 console.log("Fractional part of a number: " + Frac);
