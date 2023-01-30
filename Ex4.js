28790b69d2e53e912559c3f430d8e01cb347afb9
EGAProg <gameswithiy@gmail.com>
2023-01-30 19:10:20 +0300

    Умник

--- Ex4.js
+++ Ex4.js
@@ -11,6 +11,6 @@ let num3 = Math.abs(Math.pow(number3, 3));
 let average = (num1 + num2 + num3) / 3;
 console.log("Averange: ",average);
 
-let powAver = Math.pow(number1 + number2 + number3, -3);
+let powAver = Math.pow(((number1 ** 2) * (number2 ** 2) * (number3 ** 2)), -3);
 console.log("Power averange: ",powAver);
 
