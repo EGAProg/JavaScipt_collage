f264692db80c40fa07ed813e9a0e4208bb51a198
Egor <93545142+EGAProg@users.noreply.github.com>
2023-01-31 08:19:31 +0300

    Update Ex1.js

--- Ex1.js
+++ Ex1.js
@@ -4,12 +4,12 @@ console.log("...First way...\n");
 const number = Math.floor(Math.random() * 899) + 100;
 let StrNum = number.toString(); // Переводим Int в String для того, чтобы разбить строку на массив
 console.log(number);
-let PowRez = 1; // = 1 для того, чтобы не было деления на нуль
+let MultRez = 1; // = 1 для того, чтобы не было умножения на нуль
 let SumRez = 0;
 for (let i = 0; i < StrNum.length; i++) {
     PowRez = PowRez * Number(StrNum[i]); // Number() переводит String в Int
 }
-console.log("Power:",PowRez);
+console.log("Multiply:",PowRez);
 
 for (let i = 0; i < StrNum.length; i++) {
     SumRez = SumRez + Number(StrNum[i]); // Number() переводит String в Int
@@ -21,6 +21,6 @@ let num1 = number % 10; // Единицы
 let num2 = Math.floor((number % 100) / 10); // Десятки
 let num3 = Math.floor(number / 100); // Сотни
 let sum = num1 + num2 + num3;
-let pow = num1 * num2 * num3;
+let mult = num1 * num2 * num3;
 console.log("SUM:",sum);
-console.log("POW:",pow);
+console.log("MULT:",pow);
