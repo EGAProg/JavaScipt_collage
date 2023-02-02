612ebb338d4853bb77bfb72f1c990191eff05029
EGAProg <gameswithiy@gmail.com>
2023-02-02 13:20:10 +0300

    Лаба 3

--- Lab1/Lab2.js
+++ Lab1/Lab2.js
@@ -3,23 +3,16 @@
 function FitstEx() {
     const password = "938";
     const min = 4;
-
-    password.length > min && (password.includes("_") == true || password.includes("-"))? console.log("Пароль надежный") : console.log("Пароль ненадежный");
+    console.log(password.includes("_"));
+    password.length > min && (password.includes("_") || password.includes("-")) ? console.log("Пароль надежный") : console.log("Пароль ненадежный");
 }
 
 function SecondEx() {
     const name = "Egor";
     const surname = "Gysev";
-    let firstName = name[0].toUpperCase() + name.slice(1).toLowerCase()
-    let lastName = surname[0].toUpperCase() + surname.slice(1).toLowerCase()
-    console.log(firstName);
-    console.log(lastName);
-    if (name == firstName && surname == lastName) {
-        console.log("Без изменений");
-    }
-    else {
-        console.log("Изменено");
-    }
+    let firstName = name[0].toUpperCase() + name.slice(1).toLowerCase();
+    let lastName = surname[0].toUpperCase() + surname.slice(1).toLowerCase();
+    if (name == firstName && surname == lastName) { console.log("Без изменений"); } else { console.log("Изменено"); }
 }
 FitstEx();
 SecondEx();
