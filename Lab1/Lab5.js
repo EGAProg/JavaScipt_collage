1b52ffea5762a4e2ecb803075e789b8101054eb8
EGAProg <gameswithiy@gmail.com>
2023-02-28 11:13:38 +0300

    Lab5 FIX

--- Lab1/Lab5.js
+++ Lab1/Lab5.js
@@ -20,25 +20,27 @@ function calculate(sum, stuffCount, promo = null) {
     let persent;
     let meh;
     if (promo === "ДАРИМ300") {
-        if(sum <= 300) persent = 0;
-        else persent = sum - 300;
+        if(sum <= 300) sum = 0;
+        else sum -= 300;
     }
     if (stuffCount >= 10) {
         // 5% OFF
         persent = (sum * 5) / 100;
-
+        sum -= persent;
     }
     if (sum > 50000) {
         meh = sum - 50000;
         // 20% OFF
         persent = (meh * 20) / 100;
+        sum -= persent;
     }
     if (sum >= 20000 && promo === "СКИДКА15") {
         // 15% OFF
         persent = (sum * 15) / 100;
+        sum -= persent;
     }
 
-    return persent;
+    return sum;
 }
 console.log(`${calculate(40000, 12, "СКИДКА15")}`);
 
