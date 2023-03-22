0e9c2a9781dd9a25e88377ae1f8b5134288e8792
EGAProg <gameswithiy@gmail.com>
2023-03-23 01:03:03 +0300

    Homework

--- browser/timer/scripts.js
+++ browser/timer/scripts.js
@@ -5,11 +5,15 @@
 let btnCount = document.getElementById("btnCount");
 let counter = document.getElementById("counter");
 
-function btnClick() {    
-    let intervalId = setInterval(function() { 
-        counter.value -= 1; 
-        if (counter.value == 0) clearInterval(intervalId);
-    }, 1000);
+function btnClick() {
+    if (counter.value != 0) {  
+        let intervalId = setInterval(function() { 
+            counter.value -= 1; 
+            if (counter.value == 0) {
+                clearInterval(intervalId);
+            }
+        }, 1000);
+    }
 }
 
 function buttonCreate(name) {
@@ -19,7 +23,29 @@ function buttonCreate(name) {
     btnCount.addEventListener("click", btnClick);
 }
 
+// Задание 2
+
+let timer = document.getElementById("timer");
+let timeInput = document.createElement("timeInput");
+
+function inputUpdate() {
+    let timerId
+    const tick = 300;
+    let tickTime = tick;
+    if (tickTime != 0) {
+        timerId = setTimeout(function() {
+            timer = timeInput;
+            if (timer != timeInput) timer = timeInput;
+            tickTime-=1;
+        }, tick);
+    }
+    else {
+        clearTimeout(timerId);
+    }
+}
+
 function main() {
     buttonCreate("Timer");
+    inputUpdate();
 }
 main();
