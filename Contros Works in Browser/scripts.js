630d9ce5ef46c67aa076b72034d459f757fc16d2
EGAProg <gameswithiy@gmail.com>
2023-03-23 11:35:11 +0300

    CP2

--- Contros Works in Browser/scripts.js
+++ Contros Works in Browser/scripts.js
@@ -22,9 +22,30 @@ function createLoginForm(){
 // Задание 2
 
 
+let otvet = Math.floor(Math.random() * 10)
+let input = prompt("Введите число")
 
+function games() {
+  
+  if (input === null) {
+    return;
+  } else if (parseInt(input) > otvet) {
+    input = prompt("Меньше!")
+    games();
+  } else if (parseInt(input) < otvet) {
+    input = prompt("Больше!")
+    games();
+  } else if (parseInt(input) === otvet) {
+    alert("Правильно!")
+  }else if (isNaN(parseInt(input))) {
+    input = prompt("Не число!")
+    games();
+  }
+
+}
 
 function main() {
     createLoginForm();
+    games();
 }
 main();
