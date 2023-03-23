80e3994f7de0a4ebe7473b200c0d4d2d1a628d0a
EGAProg <gameswithiy@gmail.com>
2023-03-23 11:31:44 +0300

    CP2

---
+++ Contros Works in Browser/scripts.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let usersArray= [   
+                    {name:"John", login:"OPPYM", password:"3223322"}, 
+                    {name:"Max", login:"Maxwell", password:"paninpes111"}, 
+                    {name:"Annet", login:"BeutyAnne", password:"qwerty"},
+                ];
+
+function createLoginForm(){
+    let uname = String(prompt("Enter your username", ''));
+    let passwd = String(prompt("Enter your password", ''));
+    console.log(uname, passwd);
+
+    let user = usersArray.find(u => u.login === uname && u.password === passwd);
+    if (user) {
+        alert(`Добро пожаловать, ${user.name}!`);
+    } else {
+        alert('Неверный логин или пароль');
+    }
+}
+
+function main() {
+    createLoginForm();
+}
+main();
