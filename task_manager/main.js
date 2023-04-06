901848a71fc5b566c8d2d10878c23210e14a0366
EGAProg <gameswithiy@gmail.com>
2023-04-06 14:48:55 +0300

    v0.9-2

--- task_manager/main.js
+++ task_manager/main.js
@@ -2,7 +2,7 @@
 let Body = document.body;
 
 class Storage { // конструктор объекта для хранения в localstorage
-    constructor(value = "", complited = false) {
+    constructor(value, complited=false) {
         this.value = value;
         this.complited = complited;
     }
@@ -23,74 +23,86 @@ let daylyObj = [
     }
 ];
 
-function loadTasks(name) { // ERR:Cannot read properties of undefined (reading 'value')
+function loadTasks(name, daylyarr=null) { // ERR:Cannot read properties of undefined (reading 'value')
     console.log("Loading tasks...");
     console.log(name);
     let counter = ("Counter"+name)
     console.log(arrayOfObj.length);
-    console.log(daylyTasksArray)
+  
     let taskPlace = document.querySelector(`.tasks`);
-    for (let i = 0; i < daylyObj.length; i++) {
-        taskPlace.innerHTML += `
-            <div class="task"">
-                <span class="taskname">
-                    ${daylyObj[i].value}
-                </span>
-                <button type="button" id="btnDone" class="btn btn-done">Done</button>
-                <button type="button" id="btnDel" class="btn btn-del">Delete</button>
-            </div>`;
-        controlBtns(name, arrayOfObj, daylyObj);
-    }
-    if (localStorage.length > 0 && name in localStorage) {
-        for (let j = 0; j < counter.length; j++) { // array of tasks = 0, почему?
-            let task = JSON.parse(localStorage.getItem(name)); // Возвращает объект
-            let val = Object.values(task);
+    if(daylyarr != null) {
+        for (let i = 0; i < daylyarr.length; i++) {
             taskPlace.innerHTML += `
                 <div class="task"">
                     <span class="taskname">
-                        ${val[j].value}
+                        ${daylyarr[i].value}
                     </span>
                     <button type="button" id="btnDone" class="btn btn-done">Done</button>
                     <button type="button" id="btnDel" class="btn btn-del">Delete</button>
                 </div>`;
-            controlBtns(name, arrayOfObj, task);
+            controlBtns(name, arrayOfObj, daylyObj);
+        }
+    }
+    if (localStorage.length > 0) {
+        try {
+            let task = JSON.parse(localStorage.getItem(name)); // Возвращает объект
+            console.log(task);
+            console.log(task.length+1);
+            for (let j = 0; j < task.length; j++) { 
+                taskPlace.innerHTML += `
+                    <div class="task"">
+                        <span class="taskname">
+                            ${task[j].value}
+                        </span>
+                        <button type="button" id="btnDone" class="btn btn-done">Done</button>
+                        <button type="button" id="btnDel" class="btn btn-del">Delete</button>
+                    </div>`;
+                controlBtns(name, arrayOfObj, task);
+            }
+        } catch (ex) {
+            console.error(ex);
         }
     }
 }
 
-function controlBtns(name, array, obj=null, value) {
+function controlBtns(name, array, obj, input) {
     let counterName = ("Counter"+name);
     let taskDel  = document.querySelectorAll('#btnDel');
     let taskDone = document.querySelectorAll('#btnDone');
-    for (let i = 0; i < taskDel.length; i++) {
+    for (let i = 0; i < taskDel.length; i++) { // delete task
         taskDel[i].onclick = function() {
         let result = confirm("Delete task?");
             if(result) {
                 this.parentNode.remove();
                 let task = JSON.parse(localStorage.getItem(name));
                 let counter = JSON.parse(localStorage.getItem(counterName));
+                parseInt(counter);
+                counter -= 1;
                 localStorage.removeItem(counterName);
-                JSON.stringify(counterName, JSON.stringify(counter-1));
-                let indexArray = task.indexOf(obj);
-                if (indexArray > -1) { // only splice array when item is found
-                    array.splice(index, 1); // 2nd parameter means remove one item only
-                  }
+                if (counter < 0) {
+                    counter = 0;
+                    localStorage.setItem(counterName, counter);
+                }
+                else {
+                    localStorage.setItem(counterName, counter);
+                }
+
+                let indexArray = task.indexOf(obj[i]);
+                let removed = array.splice(indexArray, 1);
                 console.log(array);
-                localStorage.setItem(name, JSON.stringify(array));
+                localStorage.setItem(name, JSON.stringify(removed));
                 
             }
         }
     }
-    for (let i = 0; i < taskDone.length; i++) {
+    for (let i = 0; i < taskDone.length; i++) { // complete task
         taskDone[i].onclick = function() {
             this.parentNode.style.backgroundColor = "#47ff8e"
-            let done = JSON.parse(localStorage.getItem(name));
-            localStorage.removeItem(name);
             
-            done.value = value;
-            done.complited = true;
-            arrayOfObj.push(done)
-            localStorage.setItem(name, JSON.stringify(arrayOfObj));
+            let done = JSON.parse(localStorage.getItem(name)); // null
+            localStorage.removeItem(name);
+            let holder = new Storage(input, true);
+            localStorage.setItem(name, JSON.stringify(holder));
         }
     }
 }
@@ -105,7 +117,8 @@ function placeTask(taskPlace, input, name) {
             <button type="button" id="btnDel" class="btn btn-del">Delete</button>
         </div>`;
         
-        let lstorage = new Storage(input.value);
+        let lstorage = new Storage(input.value);   
+        let outValue = input.value;
         arrayOfObj.push(lstorage)
         localStorage.setItem(name, JSON.stringify(arrayOfObj)); //send array of Objects to localStorage
         for (let i = 0; i < arrayOfObj.length; i++) {
@@ -113,17 +126,15 @@ function placeTask(taskPlace, input, name) {
         }
         console.log(input.value)
         input.value = '';
-        controlBtns(name, arrayOfObj, lstorage, input.value);
+        controlBtns(name, arrayOfObj, lstorage, outValue);
 }
-
-// AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA, CYKA!
 
 function createToDo(ownerName) {
     Body.innerHTML += `
         <div class="Wrapper inner">
             <div class="placeholder">
                 <input type="text" class="hole" id="${ownerName}" placeholder="Place task">
-                <button type="submit" id="placeBtn${ownerName}" class="btn" >Place</button>
+                <button type="submit" id="placeBtn${ownerName}" class="btn" disabled>Place</button>
                 <p>${ownerName}</p>
             </div>
             <div class="tasks" id="${ownerName}"></div>
@@ -131,13 +142,18 @@ function createToDo(ownerName) {
     let btnPlace = document.querySelector(`#placeBtn${ownerName}`);
     let placeholder = document.querySelector(`#${ownerName}`);
     let taskPlace = document.querySelector(`.tasks`);
-    document.addEventListener("DOMContentLoaded", () => {
-        loadTasks(ownerName, daylyTasksArray);
+
+    window.onload = loadTasks(ownerName, daylyObj);
+    document.addEventListener("input", function() {
+        if (placeholder.value != "") {
+            btnPlace.removeAttribute("disabled");
+        } else {
+            btnPlace.setAttribute("disabled", "disabled");
+            
+        }
     });
-
-    window.onload = loadTasks(ownerName);
-
     btnPlace.addEventListener("click", () => {   
         placeTask(taskPlace, placeholder, ownerName);
+        btnPlace.setAttribute("disabled", "disabled");
     });
 }
