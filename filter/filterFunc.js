33bad1ad1a7ec75207f35f8554091d7c76be40da
EGAProg <gameswithiy@gmail.com>
2023-04-06 06:12:51 +0300

    filter rewrited func

--- filter/filterFunc.js
+++ filter/filterFunc.js
@@ -2,23 +2,25 @@
 
 let objects = [
     { name: 'Василий',
-      surname: 'Васильев' },
+      surname: 'Васильев' 
+    },
     { name: 'Иван',
-      surname: 'Иванов' },
+      surname: 'Иванов' 
+    },
     { name: 'Пётр',
-      surname: 'Петров' }
-  ];
+      surname: 'Петров' 
+    }
+];
   
 function filter(objects, key, value) {
-    const result = [];
+    let result = [];
     for (let i = 0; i < objects.length; i++) {
-      const obj = objects[i];
+        let obj = objects[i];
   
-      if (obj[key] === value) result.push(obj);
+        if (obj[key] === value) result.push(obj);
     }
-
     return result;
 }
   
-const filteredArray = filter(objects, 'name', 'Иван'); 
+let filteredArray = filter(objects, 'name', 'Иван'); 
 console.log(filteredArray); 
