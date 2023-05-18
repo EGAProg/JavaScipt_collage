7c8e79362368112e545d0b299fa803adff3d5dc3
EGAProg <gameswithiy@gmail.com>
2023-05-18 14:33:44 +0300

    Bug fix

--- cardGame/main.js
+++ cardGame/main.js
@@ -6,8 +6,6 @@ import { drawGameObjects } from "./draw.js";
 
 let arrayOfNumber = ['pink', 'blue', 'pink', 'blue', 'pink', 'blue', 'pink', 'blue'];
 let wrapper = document.querySelector(".main");
-let timer = document.querySelector(".timer");
-let timerID;
 
 function startGame() {
     console.log(shuffle(arrayOfNumber));
