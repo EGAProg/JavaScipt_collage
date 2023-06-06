81dfdcd5728fb716aa118f692943663058d2e9da
= <=>
2023-06-06 09:18:55 +0300

    GUGU GAGA

--- SQLServer/backend/database.js
+++ SQLServer/backend/database.js
@@ -1,3 +1,11 @@
 const API_ADDR = "https://localhost:3306"
 const mysql = require("mysql2");
 const express = require("express");
+
+const pool = mysql.createPool({
+    connectionLimit: 5,
+    host: "localhost",
+    user: "root",
+    database: "site",
+    password: "ytevt.ghblevsdfnmgfhjkb"
+});
