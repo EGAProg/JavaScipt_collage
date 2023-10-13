0c9fd89c4fa8023de8bfa2aaef75918c76d31dfa
EGAProg <gameswithiy@gmail.com>
2023-10-13 15:00:42 +0300

    Messenger

--- ContractFrontEnd/app.js
+++ ContractFrontEnd/app.js
@@ -2,7 +2,7 @@ const history = document.querySelector('.history');
 const dropdownSender = document.querySelector('#acc');
 const dropdown = document.querySelector('#resiever');
 
-const contractAddress = "0x620ED615CFAC1888b41fe5Ffb03f4Ba16ef9fBcc";
+const contractAddress = "0x74e66285b7957309c664E565945Faf5f91e0411c";
 const abi = [
   {
     inputs: [
@@ -52,7 +52,7 @@ const abi = [
 
 
 let myContract, accounts, currentAccount;
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
 const contracAdmin = web3.eth.accounts.wallet.add('0x817df4e4f98bfd8dbbe0f5f321c77254b954f725aef0a7996ac8d14092793fb6');
 
 async function getAccounts() {
@@ -89,34 +89,39 @@ myContract = new web3.eth.Contract(abi, contractAddress);
 console.log(myContract);
 
 async function sendMoney() {
-  const history = document.querySelector('.history');
   const EthValue = document.querySelector("#sendMoney").value;
   const KeyCode = document.querySelector("#keySend").value;
-  
   const Resiever = dropdown.options[dropdown.selectedIndex].text;
   const Sender = dropdownSender.options[dropdownSender.selectedIndex].text;
-  console.log(Sender);
-  console.log(Resiever);
-
-  await myContract.methods.Deposit(KeyCode, Resiever).send({ from: Sender, value: (EthValue * 10**18), gas: 220000}); 
-  console.log("Transaction: ", await myContract.methods.Deposit(KeyCode, Resiever).send({ from: Sender, value: (EthValue * 10**18), gas: 220000}));
-
+  await myContract.methods.Deposit(KeyCode, Resiever).send({ from: Sender, value: (EthValue * 10**18), gas: 220000});
   const hisSpan = document.createElement('span');
+  const br = document.createElement('br');
   hisSpan.className = 'hisSpan';
   hisSpan.innerText = `${Sender} -> ${Resiever} ETH: ${EthValue}.`;
   history.appendChild(hisSpan);
+  history.appendChild(br);
 }
 
 async function cancelTransaction(){
+  const br = document.createElement('br');
   const keyDec = document.querySelector('#KeyDecline');
   const Sender = dropdownSender.options[dropdownSender.selectedIndex].text;
   await myContract.methods.CancelTransaction(KeyCode).send({ from: Sender, gas: 220000}); 
   hisSpan.className = 'hisSpan';
   hisSpan.innerText = `${Sender} canseled tranzaction.`;
   history.appendChild(hisSpan);
+  history.appendChild(br);
 }
-
+/*
+  1) Блок от кого, кому, сколько, статус
+  От меня:
+    1. Send money
+    2. Cancel tranzaction
+  Мне:
+    1. Resieve money
+  */
 async function getTransaction() {
+  const br = document.createElement('br');
   const KeyCode = document.querySelector("#KeyGet").value;
   const Sender = dropdown.options[dropdown.selectedIndex].text;
   await myContract.methods.getTransact(KeyCode).send({ from: Sender, gas: 220000}); 
@@ -124,4 +129,5 @@ async function getTransaction() {
   hisSpan.className = 'hisSpan';
   hisSpan.innerText = `${Sender} resieved ETH.`;
   history.appendChild(hisSpan);
+  history.appendChild(br);
 }
--- ContractFrontEnd/contract.sol
+++ ContractFrontEnd/contract.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >= 0.7.0 < 0.9.0;
-contract tranzaction {
+contract transaction {
     struct Users{
         address addr;
         string uName;
@@ -13,6 +13,7 @@ contract tranzaction {
         uint value;
     }
     struct Transaction{
+        uint id;
         string keyWord;
         address sender;
         address resiever;
@@ -27,55 +28,57 @@ contract tranzaction {
     Users[] public users;
     
     mapping(address => Users) private user;
-    mapping(string => Transaction) private transaction;
+    //mapping(string => Transaction) private transaction;
     mapping(string => PremakeTransaction) private premakeTransactions;
+
+    // struct TransactionMapAddr {
+    //     uint256 id;
+    //     mapping(string => Transaction) transaction;
+    // }
+    //TransactionMapAddr[] public transactionsAddr;
     constructor() { 
         // Заранее заготовленные переводы
         //premakeTransactions["Present"] = PremakeTransaction("Present", 0x17F6AD8Ef982297579C203069C1DbfFE4348c372, (0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, 20);
 
         // Пользователи
-        // user[0x5B38Da6a701c568545dCfcB03FcB875f56beddC4] = Users(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, "Ivan", true);
-        // user[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2] = Users(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, "Egor", true);
-        // user[0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db] = Users(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db, "Max", false);
-        // user[0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB] = Users(0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB, "Vetal", false);
-        // user[0x617F2E2fD72FD9D5503197092aC168c91465E7f2] = Users(0x617F2E2fD72FD9D5503197092aC168c91465E7f2, "Kirill", false);
-        // user[0x17F6AD8Ef982297579C203069C1DbfFE4348c372] = Users(0x17F6AD8Ef982297579C203069C1DbfFE4348c372, "Vova", false);
+        user[0x5B38Da6a701c568545dCfcB03FcB875f56beddC4] = Users(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, "Ivan", true);
+        user[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2] = Users(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, "Egor", true);
+        user[0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db] = Users(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db, "Max", false);
+        user[0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB] = Users(0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB, "Vetal", false);
+        user[0x617F2E2fD72FD9D5503197092aC168c91465E7f2] = Users(0x617F2E2fD72FD9D5503197092aC168c91465E7f2, "Kirill", false);
+        user[0x17F6AD8Ef982297579C203069C1DbfFE4348c372] = Users(0x17F6AD8Ef982297579C203069C1DbfFE4348c372, "Vova", false);
     }
     
     // 
 
-    function Deposit(string memory _keyCode, address _Resiever) public payable {
+    function Deposit(uint id, string memory _keyCode, address _Resiever) public payable {
         //require(user[msg.sender]); // Затравочка на то, что переводить могут только зарегестрированные пользователи
         require(msg.value > 0, "0 in Value");
-        //require(msg.value <= msg.sender.balance * 10**18, "You Have No Such Money");
-        transaction[_keyCode].confermed == false;
-        transaction[_keyCode].sender = msg.sender;
-        transaction[_keyCode].keyWord = _keyCode;
-        transaction[_keyCode].value = msg.value;
-        transaction[_keyCode].resiever = _Resiever;
+        transactions.push(Transaction(id, _keyCode, msg.sender, _Resiever, msg.value,  false));
     }
 
-    function CancelTransaction(string memory _keyCode) public payable{
-        require(transaction[_keyCode].confermed == false);
-        require(transaction[_keyCode].sender == msg.sender);
-        payable(msg.sender).transfer(transaction[_keyCode].value); 
+    function CancelTransaction(uint id, string memory _keyCode) public payable{
+        require(transactions[id].confermed == false);
+        require(keccak256(abi.encodePacked(_keyCode)) == keccak256(abi.encodePacked(transactions[id].keyWord)));
+        require(transactions[id].sender == msg.sender);
+        payable(msg.sender).transfer(transactions[id].value); 
     }
     
     function PreMake(string memory _Type) public payable { // Заготовка для заготовленных транзакций
         payable(premakeTransactions[_Type].resiever).transfer(premakeTransactions[_Type].value);
     }
 
-    function getTransact(string memory _keyCode) public payable {
-        require(msg.sender == transaction[_keyCode].resiever);
-        require(transaction[_keyCode].value > 0, "0 in value");
-        if (keccak256(abi.encodePacked(_keyCode)) == keccak256(abi.encodePacked(transaction[_keyCode].keyWord))) {
-            payable(msg.sender).transfer(transaction[_keyCode].value);
+    function getTransact(uint id, string memory _keyCode) public payable {
+        require(msg.sender == transactions[id].resiever);
+        require(transactions[id].value > 0, "0 in value");
+        if (keccak256(abi.encodePacked(_keyCode)) == keccak256(abi.encodePacked(transactions[id].keyWord))) {
+            payable(msg.sender).transfer(transactions[id].value);
             
         }
         else {
             
-            payable(transaction[_keyCode].sender).transfer(transaction[_keyCode].value); // Доработать возврат средств при неправильном ключе
+            payable(transactions[id].sender).transfer(transactions[id].value); // Доработать возврат средств при неправильном ключе
         }
-        transaction[_keyCode].confermed = true;
+        transactions[id].confermed = true;
     }
 }
